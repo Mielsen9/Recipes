b15267448bf5855aa4a51abb7e826cae393fc02b
Petro <worldinthegame@gmail.com>
2025-03-06 09:33:30 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+// Type
+
+// App
+export const App = () => {
+    // State
+
+    // Logic
+
+    // Return
+    return (
+        <div>
+
+        </div>
+    );
+};
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import  "./scss/style.scss";
+import {App} from "./App";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const root = document.getElementById('root');
+
+if (!root) {
+    throw new Error('root nod found')
+}
+
+const container = createRoot(root)
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
+
+container.render(
+    <RouterProvider router={router}/>
+)
+
+
+
